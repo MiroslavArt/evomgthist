7c6d5c3c2f800ee93bb7205ffedd141b8b414733
Ruslan Flato <platonov@itrack.ru>
2020-11-05 10:06:03 +0300

    VAR2 вместо создания/копирования текста - установка ссылки на текст  task_50795

--- local/modules/itrack.custom/lib/application.php
+++ local/modules/itrack.custom/lib/application.php
@@ -6,6 +6,71 @@ use Bitrix\Main\Config\Option;
 use Bitrix\Main\EventManager;
 use Bitrix\Main\Page\Asset;
 
+function funcOnAfterCrmTimelineCommentAdd($ID)
+{
+
+	$checkData = \Bitrix\Crm\Timeline\Entity\TimelineTable::getList(
+		array(
+			'select' => [
+				'ID'
+				, 'TYPE_ID'
+				, 'BINDING.ENTITY_TYPE_ID'
+				,'BINDING.ENTITY_ID'
+				// ,'AUTHOR_ID'
+				// ,'COMMENT'
+				// ,'SETTINGS'
+			]
+		, 'order' => ['CREATED' => 'desc']
+		, 'filter' => array(
+			'=BINDING.ENTITY_TYPE_ID' => $ownerTypeID = \CCrmOwnerType::Deal
+		, '=ID' => $ID
+			// '=BINDING.ENTITY_ID' => $ownerID =999,
+			// 'BINDINGS.ENTITY_ID' => 111388
+		),
+			'runtime' => array(
+				new \Bitrix\Main\Entity\ReferenceField(
+					'BINDING',
+					'\Bitrix\Crm\Timeline\Entity\TimelineBindingTable',
+					array("=ref.OWNER_ID" => "this.ID"),
+					array("join_type" => "INNER")
+				)
+			),
+			'limit' => 2
+		)
+	);
+	$strIdDeal = $checkData->fetch()['CRM_TIMELINE_ENTITY_TIMELINE_BINDING_ENTITY_ID'];
+
+	if(!empty($strIdDeal)) {
+		$currentDbResult = \CCrmDeal::GetList(
+			[],
+			['=ID' => $strIdDeal, 'CHECK_PERMISSIONS' => 'N'],
+			['CONTACT_ID'],
+			false,
+			);
+		$strIdContact = $currentDbResult->Fetch()['CONTACT_ID'];
+
+		$fields = array(
+			'OWNER_ID' => $ID,
+			'ENTITY_TYPE_ID' => \CCrmOwnerType::Contact,
+			'ENTITY_ID' => $strIdContact,
+		);
+
+		$connection = \Bitrix\Main\Application::getConnection();
+		$queries = $connection->getSqlHelper()->prepareMerge(
+			'b_crm_timeline_bind',
+			array('OWNER_ID', 'ENTITY_TYPE_ID', 'ENTITY_ID'),
+			$fields,
+			$fields
+		);
+
+		foreach ($queries as $query) {
+			$connection->queryExecute($query);
+		}
+	}
+
+}
+
+
 class Application
 {
     const DEAL_CATEGORY_EDUCATION_ID = 47;
@@ -37,7 +102,9 @@ class Application
         $eventManager->addEventHandler('main','OnProlog', ['\iTrack\Custom\Handlers\Main','onProlog']);
         $eventManager->addEventHandler('main','OnEpilog', ['\iTrack\Custom\Handlers\Main','onEpilog']);
         $eventManager->addEventHandler('im','OnBeforeMessageNotifyAdd', ['\iTrack\Custom\Handlers\Im','onBeforeMessageNotifyAdd']);
-        $eventManager->addEventHandler("crm", "OnAfterCrmTimelineCommentAdd", ['\iTrack\Custom\Handlers\Crm','funcOnAfterCrmTimelineCommentAdd']);
+        // $eventManager->addEventHandler("crm", "OnAfterCrmTimelineCommentAdd", ['\iTrack\Custom\Handlers\Crm','funcOnAfterCrmTimelineCommentAdd']);
+        $eventManager->addEventHandler("crm", "OnAfterCrmTimelineCommentAdd", funcOnAfterCrmTimelineCommentAdd);
+
 
     }
 
