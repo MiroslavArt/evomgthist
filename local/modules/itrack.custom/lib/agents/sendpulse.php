6056eeb18902030ad5b95bcd73495b92a03c993d
Miros <lya-job@yandex.ru>
2021-04-16 14:31:14 +0300

    лог отправки файла

--- local/modules/itrack.custom/lib/agents/sendpulse.php
+++ local/modules/itrack.custom/lib/agents/sendpulse.php
@@ -21,4 +21,39 @@ class Sendpulse
         }
         return '\iTrack\Custom\Agents\Sendpulse::reindex();';
     }
+
+    public static function senddailylog()
+    {
+
+        $sendFilePath = $_SERVER['DOCUMENT_ROOT']."/sendpulse/".date('d.m.Y').".log";
+
+        $fileId = \CFile::SaveFile(
+            array(
+                "name" => date('d.m.Y').".log",
+                "tmp_name" => $sendFilePath,
+                "old_file" => "0",
+                "del" => "N",
+                "MODULE_ID" => "",
+                "description" => "",
+            ),
+            'sendpulse',
+            false,
+            false
+        );
+
+        $emails = ['marketing1.mow@evomgt.org', 'segay@itrack.ru'];
+
+        foreach ($emails as $email) {
+            $eventFields = [
+                "EMAIL_TO" => $email,
+                "DAY" => date('d.m.Y')
+            ];
+            \CEvent::Send('SENDPULSE_LOG_SEND', SITE_ID, $eventFields, 'N','',array($fileId));
+        }
+
+        return '\iTrack\Custom\Agents\Sendpulse::senddailylog();';
+    }
+
+
+
 }
